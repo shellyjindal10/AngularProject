d4c1c46658cb0f23296aea7103d20375867941a7
shelly jindal <shellyjindal@shellys-MacBook-Pro.local>
2015-04-03 14:04:58 -0700

    checkins

--- project1/js/app1.js
+++ project1/js/app1.js
@@ -1,34 +1,43 @@
-(function(){
-	var app = angular.module('store',[ ]);
-	app.controller('MultipleStoreController',function(){
-        this.product = gems;
-	});
+// (function(){
+// 	var app = angular.module('store',[ ]);
+// 	app.controller('MultipleStoreController',function(){
+//         this.product = gems;
+// 	});
 
-	var gems = [
-	     {
-			name:'sudip Gautam',
-			age:30,
-	        company:"Yahoo",
-	        images:[
-		        {
-		        	full:'img/watch.jpg'
-		        }
-	        ]
-		},
-		{
-	        name:'yamini Yadav',
-			age:28,
-	        company:"Google",
-		},
-		{
-	        name:'shelly jindal',
-			age:32,
-	        company:"apple",
-		},
-		{
-	        name:'abc xyz',
-			age:90,
-	        company:"facebook",
-		}
-	];
-})();
+// 	var gems = [
+// 	     {
+// 			name:'sudip Gautam',
+// 			age:30,
+// 	        company:"Yahoo",
+// 	        images:[
+// 		        {
+// 		        	full:'img/watch.jpg'
+// 		        }
+// 	        ]
+// 		},
+// 		{
+// 	        name:'yamini Yadav',
+// 			age:28,
+// 	        company:"Google",
+// 		},
+// 		{
+// 	        name:'shelly jindal',
+// 			age:32,
+// 	        company:"apple",
+// 		},
+// 		{
+// 	        name:'abc xyz',
+// 			age:90,
+// 	        company:"facebook",
+// 		}
+// 	];
+// })();
+(function(){
+      var app = angular.module('page',[ ]);
+      app.controller('PageController',function(){
+        this.product = context;
+	  });
+
+	  var context = {message:'hello'};
+
+ })();
