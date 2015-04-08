297843b3b8ede95d013d0303d102c16a42ab841a
shelly jindal <shellyjindal@shellys-MacBook-Pro.local>
2015-04-08 16:27:04 -0700

    for angular js implementation

---
+++ project2/js/app.js
@@ -0,0 +1,30 @@
+(function(){
+	var app = angular.module('store',[ ]);
+	app.controller('StoreController',function(){
+		this.product = data;
+	});
+	
+	var data = {
+		title:'Template',
+		heading:'MoneySplit',
+		introduction:'Split money among friends',
+		twitter:'Twitter',
+		github:'Github',
+		linkedin:'Linkedin',
+		about:'About',
+		home:'Home',
+		services:'Services',
+		contact:'Contact',
+		sectionHeading1:'Money Split',
+		sectionHeading2:'Provides you',
+		sectionParagraph:'Living with your mates? Whether its bills, takeaway or just daily expenses, MoneySplit deals with all the hassles & calculations while you sit back and relax! You can add expenses as soon as they happen.',
+		paybillsHeading1:'Pay Bills',
+		paybillsHeading2:'by PayPal',
+		paybillsParagraph:'Students & Houseshares,Group Expenses,Parties & Clubs,Camping Trips & Holidays,Gig & Theatre Tickets,Online Groceries & Shopping,Rent & Mortage Payments,Personal IOUs & more ...!',
+		makeFriendsHeading1:'Make more friends now',
+		makeFriendsParagraph1:'Forget about Excel sheets after a trip with your friends! ',
+		makeFriendsParagraph2:'Socialize more with this super easy and fast user interface.',
+		connectMeHeading:'Connect Me',
+		copyrightTitle:'Your Company 2015. All Rights Reserved'
+	}
+})();
